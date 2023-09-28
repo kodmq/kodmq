edbf2212352f4f1f614c107207be115a9b8bfdaf
akodkod <akodkod@users.noreply.github.com>
2023-09-28 01:58:41 -0700

    [kodmq-ui] Detect `next` executable path

--- packages/kodmq-ui/bin/start.js
+++ packages/kodmq-ui/bin/start.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 import { execSync } from "child_process"
 import { dirname, resolve } from "path"
@@ -7,7 +8,27 @@ import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+console.log()
+console.log("\x1b[36m%s\x1b[0m", "Starting Kodmq UI...")
+console.log()
+
 const appPath = resolve(__dirname, "../")
-const nextBinPath = resolve(appPath, "node_modules/next/dist/bin/next")
+let nextBinPath
+
+try {
+  const result = execSync("which next", { cwd: appPath, stdio: "pipe" })
+  nextBinPath = result.toString().trim()
+} catch (e) {
+  console.error("\x1b[31m%s\x1b[0m", "Error: Could not find executable for Next.js. Please, make sure you have Next.js installed in your project dependencies or globally.")
+  console.log()
+  console.log("\x1b[33m%s\x1b[0m", "To install Next.js globally run one of the following commands:")
+  console.log("npm install next -g")
+  console.log("pnpm install next -g")
+  console.log("yarn install next -g")
+  console.log("bun install next -g")
+  console.log()
+
+  process.exit(1)
+}
 
 execSync(`${nextBinPath} start`, { cwd: appPath, stdio: "inherit" })
