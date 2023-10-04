4433347c591fd6da506990b550a974bdcd6b06d1
akodkod <akodkod@users.noreply.github.com>
2023-10-04 02:41:16 -0700

    Update docs and README

--- README.md
+++ README.md
@@ -17,3 +17,21 @@
 ## Documentation
 
 You can find the documentation → [https://kodmq.com](https://kodmq.com).
+
+## Sponsor
+
+<a href="https://kvik.photos" target="_blank">
+  <picture>
+    <source srcset="./apps/docs/src/images/kvikphotos-logo.png" media="(prefers-color-scheme: light)" />
+    <source srcset="./apps/docs/src/images/kvikphotos-logo-dark-mode.png" media="(prefers-color-scheme: dark)" />
+    <img src="./apps/docs/src/images/kvikphotos-logo.png" alt="KvikPhotos" height="64" />
+  </picture>
+</a>
+
+KvikPhotos helps you share your RAW photos with friends, family and clients
+
+- Easily upload photos
+- Automatically convert them into JPEGs
+- Share your best photos and albums with a link
+
+&rarr; [https://kvik.photos](https://kvik.photos)
--- packages/ui/lib/types.ts
+++ packages/ui/lib/types.ts
@@ -1,21 +1,13 @@
 import { ComponentProps, FunctionComponent, JSX, JSXElementConstructor, PropsWithoutRef, RefAttributes, SVGProps } from "react"
 
 export type ExtendProps<T extends keyof JSX.IntrinsicElements | JSXElementConstructor<unknown>, U> = Omit<ComponentProps<T>, keyof U> & U;
-export type SetState<T> = React.Dispatch<React.SetStateAction<T>>
 
 export type StringKeys<T> = Extract<keyof T, string>
-export type DateKeys<T> = Extract<keyof T, Date>
 export type KeysOfType<T, U> = { [K in keyof T]: T[K] extends U ? K : never }[keyof T]
-
 
 type IconSVGProps = PropsWithoutRef<SVGProps<SVGSVGElement>> & RefAttributes<SVGSVGElement>
 export type IconProps = IconSVGProps & { title?: string, titleId?: string }
 export type Icon = FunctionComponent<IconProps>
 
-export type Errors = {
-  [key: string]: string
-}
-
-export type ErrorWithMessage = {
-  message: string
-}
+export type Errors = { [key: string]: string }
+export type ErrorWithMessage = { message: string }
